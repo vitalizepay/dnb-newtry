7b2bf8f5edfd237f21ee69acd27142041e92b3de
vitalizepay <vitalizepay@gmail.com>
2026-01-15 01:05:43 +0530

    Update contact number in Header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -41,9 +41,9 @@ const Header = () => {
           {/* Phone Number - Desktop with UAE Flag */}
           <div className="hidden lg:flex items-center gap-2 text-primary font-medium">
             <img src={uaeFlag} alt="UAE" className="h-4 w-auto" />
-            <a href="tel:+971522085242" className="hover:text-primary/80 transition-colors">
-              +971 52 208 5242
-            </a>
+            <a href="tel:+971585570593" className="hover:text-primary/80 transition-colors">
+              +971 58 5570593
+              </a>
           </div>
 
           {/* Mobile Menu Button */}
@@ -71,11 +71,11 @@ const Header = () => {
                 </Link>
               ))}
               <a
-                href="tel:+971522085242"
+                href="tel:+971585570593"
                 className="flex items-center gap-2 text-primary font-medium py-2"
               >
                 <img src={uaeFlag} alt="UAE" className="h-4 w-auto" />
-                +971 52 208 5242
+                +971 58 5570593
               </a>
             </nav>
           </div>
